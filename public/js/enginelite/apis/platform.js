d704e207ba54c7788acd3f3870f9e285db8c0611
Christian Joun <christian@adifferentengine.com>
2013-11-27 12:44:39 -0800

    bug fixed

--- public/js/enginelite/apis/platform.js
+++ public/js/enginelite/apis/platform.js
@@ -5,28 +5,6 @@ define(['underscore'],function(_) {
       if(config.isBuild) {
         load();
       } else {
-<<<<<<< HEAD
-        if(window.boxee) {
-          require(['enginelite/platforms/enginelite.platform.boxee'], load);
-        }
-        else if ( navigator.userAgent.search(/GoogleTV/)  > -1) {
-          require(['enginelite/platforms/enginelite.platform.googletv'], load);
-        }
-        else if ( navigator.userAgent.search(/LG Browser/) > -1 ) {
-          require(['enginelite/platforms/enginelite.platform.lg'], load);
-        }
-        else if ( navigator.userAgent.search(/Viera/) > -1 ) {
-          require(['enginelite/platforms/enginelite.platform.panasonic'], load);
-        }
-        else if ( navigator.userAgent.search(/Maple/) > -1 ) {
-          require(['enginelite/platforms/enginelite.platform.samsungmodern'], load);
-        }
-         else if ( navigator.userAgent.search(/AQUOS/) > -1 ) {
-          require(['enginelite/platforms/enginelite.platform.sharp'], load);
-        } else {
-           require(['enginelite/platforms/enginelite.platform.browser'], load);
-=======
->>>>>>> development
 
         var tests = _([
             { test:/GoogleTV/, file: 'enginelite/platforms/enginelite.platform.googletv', name: "Google TV"},
