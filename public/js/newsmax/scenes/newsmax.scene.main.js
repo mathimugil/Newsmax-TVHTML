84e812fece6abdba2ebe501a17f84c2526b4a04b
Christian Joun <christian@adifferentengine.com>
2013-12-11 14:06:29 -0800

    search keyboard now cancels fetches for grid items: issue 1558

--- public/js/newsmax/scenes/newsmax.scene.main.js
+++ public/js/newsmax/scenes/newsmax.scene.main.js
@@ -296,6 +296,7 @@ define([
                         case 'search':
                             hideGrid();
                             hideSubNav = true;
+                            cancelFetch = true;
                             keyMenu.focus();
                             break;
                     }
