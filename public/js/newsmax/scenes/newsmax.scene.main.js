991d6bcc1ea3be859e7dae86cc4863ae8127ac47
JULIE PETRARCA <jpetrarca@gmail.com>
2013-12-12 13:11:15 -0800

    1566 if search term is empty, do nothing

--- public/js/newsmax/scenes/newsmax.scene.main.js
+++ public/js/newsmax/scenes/newsmax.scene.main.js
@@ -605,6 +605,7 @@ define([
         }
 
         var runSearch = function(term) {
+            if (term === "") return;
             searchState = true;
             cancelFetch = false;
             showLoader();
